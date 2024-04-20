03d8a3df2cfd4a1b99bd79f72d4d71574c0a2181
Hezos <Nbence0620@gmail.com>
2024-04-20 16:28:57 +0200

    Updating master locally.

--- source/EvoPlanet/EvoPlanet.Server/Services/PlanetService.cs
+++ source/EvoPlanet/EvoPlanet.Server/Services/PlanetService.cs
@@ -1,10 +1,20 @@
 ﻿using EvoPlanet.Server.Models;
 using System.Text.Json;
+using MongoDB.Driver;
 
 namespace EvoPlanet.Server.Services
 {
     public class PlanetService
     {
+        private readonly IMongoCollection<Planet> _planets;
+
+        public PlanetService()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("evoPlanet");
+            _planets = database.GetCollection<Planet>("Planet");
+        }
+
         //File read
         public Planet PlanetResult()
         {
@@ -25,5 +35,12 @@ namespace EvoPlanet.Server.Services
             planet.Mass = 2;
             DataBase.SaveData("valami.json", JsonSerializer.Serialize(planet));
         }
+
+        public async Task<Planet> CreateAsync(Planet planet)
+        {
+            await _planets.InsertOneAsync(planet);
+            return planet;
+        }
+
     }
 }
