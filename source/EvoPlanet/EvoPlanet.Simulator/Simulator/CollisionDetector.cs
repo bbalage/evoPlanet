07b135eaade0c6e1222001480d1df4ce588bc2b5
Balazs Bolyki <balazs.bolyki.ext@siemens-healthineers.com>
2024-04-22 15:23:26 +0200

    Added SimulatorLibraryMVP.

---
+++ source/EvoPlanet/EvoPlanet.Simulator/Simulator/CollisionDetector.cs
@@ -0,0 +1,23 @@
+﻿/* -------------------------------------------------------------------------------------------------
+   Copyright (C) Siemens Healthcare GmbH 2024, All rights reserved. Restricted.
+   ------------------------------------------------------------------------------------------------- */
+
+using EvoPlanet.Simulator.Celestial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoPlanet.Simulator.Simulator
+{
+    public class CollisionDetector
+    {
+        public CollisionDetector() { }
+
+        public bool IsColliding(CelestialBody body1, CelestialBody body2)
+        {
+            return false;
+        }
+    }
+}
---
+++ source/EvoPlanet/sdk.Simulator.uTest/Simulator/CollisionDetectorTest.cs
@@ -0,0 +1,43 @@
+﻿/* -------------------------------------------------------------------------------------------------
+   Copyright (C) Siemens Healthcare GmbH 2024, All rights reserved. Restricted.
+   ------------------------------------------------------------------------------------------------- */
+
+using EvoPlanet.Simulator.Celestial;
+using EvoPlanet.Simulator.Simulator;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdk.SimulatorConsole.Simulator
+{
+    internal class CollisionDetectorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NonCollidingBodies_NotColliding()
+        {
+            var body1 = new CelestialBody(
+                Vector<double>.Build.DenseOfArray(new double[] { 1.0, 10.0 }),
+                Vector<double>.Build.DenseOfArray(new double[] { 1.0, 10.0 }),
+                1000,
+                100);
+            var body2 = new CelestialBody(
+                Vector<double>.Build.DenseOfArray(new double[] { 100000.0, 10.0 }),
+                Vector<double>.Build.DenseOfArray(new double[] { 1.0, 10.0 }),
+                1000,
+                100);
+            var collisionDetector = new CollisionDetector();
+
+            bool isColliding = collisionDetector.IsColliding(body1, body2);
+
+            Assert.That(isColliding, Is.True);
+        }
+    }
+}
