4069592dbd5deef781c22252c4bc55967257fc7e
Hauer Attila Árpád <h.attila.arpad@gmail.com>
2024-06-16 11:26:52 +0200

    create simulator page (canvas, redirect button)

---
+++ source/EvoPlanet/evoplanet.client/src/app/components/simulator/simulator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulator',
+  standalone: true,
+  imports: [],
+  templateUrl: './simulator.component.html',
+  styleUrls: ['./simulator.component.css']
+})
+export class SimulatorComponent {
+
+}
