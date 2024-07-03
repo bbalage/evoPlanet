2a510bb1f623e03ab1fe7d4484e8e977db5919f2
Hezos <Nbence0620@gmail.com>
2024-07-03 07:13:55 +0200

    Added basic planet moving system.

--- source/EvoPlanet/evoplanet.client/src/app/components/simulator/simulator.component.ts
+++ source/EvoPlanet/evoplanet.client/src/app/components/simulator/simulator.component.ts
@@ -1,21 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SolarSystemService } from '../../services/solar-system/solar-system.service';
-import { IdHandler, SolarSystem } from '../../types';
+import { CelestialBodyReference, Coordinate, IdHandler, SolarSystem, VelocityVector } from '../../types';
 
 @Component({
   selector: 'app-simulator',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './simulator.component.html',
   styleUrls: ['./simulator.component.css'],
   providers: [SolarSystemService]
 })
+
+
+
 export class SimulatorComponent implements OnInit {
   activatedRoute = inject(ActivatedRoute);
   handler: IdHandler = { id:'' }
   router = inject(Router);
+  Alpha: number = 0;
+  highestMass: number = 0;
+  index: number = 0;
+
+
   ngOnInit(): void {
     const solarId: string = this.activatedRoute.snapshot.params['id'];
     this.handler.id = solarId
@@ -23,12 +32,58 @@ export class SimulatorComponent implements OnInit {
       {
         next: (item) => {
           this.planetSystem = item;
+
+          this.planetSystem.CelestialBodies?.forEach(
+            (celestialbody: CelestialBodyReference) => {
+              this.positions.push(celestialbody.Coordinate);
+              this.velocities.push(celestialbody.VelocityVector);
+            }
+          );
+
         }
       }
     );
+    
+
+    this.Draw();
+
   }
+
+  Draw(): void {
+    var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("simulator-canvas");
+    var ctx: any;
+    ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Hardcoded index to be 2 but ideally it should be decided with mass
+    this.index = 0;
+    for (var i: number = 0; i < this.positions.length; i++) {
+      if (i !== this.index) {
+        ctx.beginPath();
+        ctx.arc(this.positions[i].PX + 10 * Math.cos(this.Alpha), this.positions[i].PY + 10 * Math.sin(this.Alpha), 10, 0, 2 * Math.PI);
+        ctx.stroke();
+      }
+      else
+      {
+        ctx.beginPath();
+        ctx.arc(this.positions[i].PX, this.positions[i].PY, 10, 0, 2 * Math.PI);
+        ctx.stroke();
+      }
+    }
+
+
+    this.Alpha += 30;
+    setTimeout( () => {
+      requestAnimationFrame(() => { this.Draw() });
+    }, 1000 / 5);
+  
+}
+
   planetSystem: SolarSystem = { id: ''  };
   solarService: SolarSystemService = inject(SolarSystemService);
 
+  positions: Array<Coordinate> = [];
+  velocities: Array<VelocityVector> = [];
 
 }
