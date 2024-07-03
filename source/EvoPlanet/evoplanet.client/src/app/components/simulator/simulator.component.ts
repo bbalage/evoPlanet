1ba374c8f11d0e1eb7dea82187f0a367f12ae002
Hezos <Nbence0620@gmail.com>
2024-07-03 17:24:00 +0200

    Added new drawing functions

--- source/EvoPlanet/evoplanet.client/src/app/components/simulator/simulator.component.ts
+++ source/EvoPlanet/evoplanet.client/src/app/components/simulator/simulator.component.ts
@@ -23,6 +23,13 @@ export class SimulatorComponent implements OnInit {
   Alpha: number = 0;
   highestMass: number = 0;
   index: number = 0;
+  celestialBodySprite: HTMLImageElement;
+
+  constructor() {
+    this.celestialBodySprite = new Image();
+    this.celestialBodySprite.src = "assets/logo.png";
+
+  }
 
 
   ngOnInit(): void {
@@ -50,29 +57,44 @@ export class SimulatorComponent implements OnInit {
   }
 
   Draw(): void {
-    var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("simulator-canvas");
-    var ctx: any;
-    ctx = canvas.getContext("2d");
+    const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("simulator-canvas");
+    const ctx: any = canvas.getContext("2d");
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    
     //Hardcoded index to be 2 but ideally it should be decided with mass
     this.index = 0;
-    for (var i: number = 0; i < this.positions.length; i++) {
-      if (i !== this.index) {
+    for (let i in this.positions) {
+      if (i != this.index.toString()) {
         ctx.beginPath();
-        ctx.arc(this.positions[i].PX + 10 * Math.cos(this.Alpha), this.positions[i].PY + 10 * Math.sin(this.Alpha), 10, 0, 2 * Math.PI);
-        ctx.stroke();
+       // ctx.arc(this.positions[i].PX + 10 * Math.cos(this.Alpha), this.positions[i].PY + 10 * Math.sin(this.Alpha), 10, 0, 2 * Math.PI);
+        // ctx.stroke();
+        ctx.drawImage(
+          this.celestialBodySprite,
+          this.positions[i].PX + 10 * Math.cos(this.Alpha),
+          this.positions[i].PY + 10 * Math.sin(this.Alpha),
+          20,
+          20
+        );
       }
       else
       {
         ctx.beginPath();
-        ctx.arc(this.positions[i].PX, this.positions[i].PY, 10, 0, 2 * Math.PI);
-        ctx.stroke();
+       // ctx.arc(this.positions[i].PX, this.positions[i].PY, 10, 0, 2 * Math.PI);
+        // ctx.stroke();
+        ctx.drawImage(
+          this.celestialBodySprite,
+          this.positions[i].PX,
+          this.positions[i].PY,
+          20,
+          20
+        );
       }
     }
 
 
+    //TODO:Remove setTimeOut.
     this.Alpha += 30;
     setTimeout( () => {
       requestAnimationFrame(() => { this.Draw() });
